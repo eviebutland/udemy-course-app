78a8d737875184dbc9c742a2bd536956a75b81d9
eviebutland <evie.butland@gmail.com>
2021-06-19 22:30:19 +0100

    more changes

--- App.js
+++ App.js
@@ -1,33 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import { GoalItem } from './components/GoalItem'
 import { GoalInput } from './components/GoalInput'
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([])
-
+  const [isAddMode, setIsAddMode] = useState(false)
   const addGoalToPage = (courseGoal) => {
     setCourseGoals([...courseGoals, {id: Math.random().toString() , value: courseGoal}])
-    
+    setIsAddMode(false)
   }
   const removeItemFromList = (goalId) => e => {
     // filter returns an array
     // filter will remove items if they do not match 
     return setCourseGoals(courseGoals => courseGoals.filter(goal => goal.id !== goalId))
   }
+  const handleCloseModel = () => {
+    setIsAddMode(false)
+  }
 
   return (
     <View style={styles.container}>
       <Text>Which is in a view, like a div!</Text>
+      <Button title="Add new goal" onPress={() => setIsAddMode(true)}/>
       <GoalInput 
+        isAddMode={isAddMode}
         buttonTitle="Add"
-        addGoalToPage={addGoalToPage}/>
+        addGoalToPage={addGoalToPage}
+        onCloseModel={handleCloseModel}/>
       <FlatList style={styles.list} 
         data={courseGoals} 
         keyExtractor={(item, key) => item.key}
         renderItem={ itemData => 
-          <GoalItem  title={itemData.item.value} onDelete={removeItemFromList(itemData.item.id)}/>
+          <GoalItem id={itemData.item.id} title={itemData.item.value} onDelete={removeItemFromList(itemData.item.id)}/>
       }></FlatList>
       <StatusBar style="auto" />
     </View>
--- components/GoalInput.jsx
+++ components/GoalInput.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { StyleSheet, View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Modal } from 'react-native'
 
 export const GoalInput = (props) => {
   const [courseGoal, setCourseGoal] = useState('')
@@ -7,26 +7,42 @@ export const GoalInput = (props) => {
   const handleChange = (e) => {
     setCourseGoal(e)
   }
+  const addNewGoalHandler = () => {
+    props.addGoalToPage(courseGoal)
+    setCourseGoal('')
+  }
 
   return (
-    <View style={styles.textContainer}>
-      <TextInput style={styles.textInput} placeholder="Course Goal" onChangeText={handleChange} value={courseGoal}></TextInput>
-      <Button title={props.buttonTitle} onPress={() => props.addGoalToPage(courseGoal)}/>
-      {/* Above in addGoalToPage passes up a child property to a parent by using an anonomous function (it does not get rendered every call now) */}
-    </View>
+    <Modal visible={props.isAddMode} animationType='fade'>
+        <View style={styles.container}>
+          <TextInput style={styles.textInput} placeholder="Course Goal" onChangeText={handleChange} value={courseGoal}></TextInput>
+          <View style={styles.buttonContainer}>
+            <Button title={props.buttonTitle} onPress={addNewGoalHandler}/>
+            {/* Above in addGoalToPage passes up a child property to a parent by using an anonomous function (it does not get rendered every call now) */}
+            <Button title="Cancel" onPress={() => props.onCloseModel()} color="red"/>
+          </View>
+        </View>
+    </Modal>
   )
 }
 
 const styles = StyleSheet.create({
-  textContainer: {
+  container: {
     padding: 40, 
+    alignItems: 'center',
     flexDirection: 'row', 
-    justifyContent: 'space-between'
+    justifyContent: 'center',
+    flex: 1,
   },
   textInput: {
     borderBottomColor: 'black', 
     borderWidth: 1, 
     padding: 10, 
-    width: '100%'
+    width: '90%'
   },
+  buttonContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: '80%'
+  }
 })
